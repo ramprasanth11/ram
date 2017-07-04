a93ac81db5945138aacc3482b2728ffe9d1e1d37
ramprasanth11 <ram.prasanthcse@ksrce.ac.in>
2017-07-04 11:24:23 +0530

    Add files via upload

---
+++ pgm17.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+  int a,b,c;
+  printf("\n enter the value of a,b,c:");
+  scanf("%d %d %d",&a,&b,&c);
+  if(a>b&&a>c)
+  {
+      printf("a is greater");
+  }
+  else if(b>a&&b>c)
+  {
+      printf("b is greater");
+  }
+  else{
+    printf("c is greater");
+  }
+  return 0;
+}

44cc3e2cd27889a53d898861fcc278dd774b6743
ramprasanth11 <ram.prasanthcse@ksrce.ac.in>
2017-07-04 14:18:27 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(""+fact);
+
+	}
+
+}

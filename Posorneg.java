3106a80abfdad23aa153cb15ead858e6edab559a
ramprasanth11 <ram.prasanthcse@ksrce.ac.in>
2017-07-04 10:20:42 +0530

    Add files via upload

---
+++ Posorneg.java
@@ -0,0 +1,22 @@
+package guvi;
+import java.util.Scanner;
+public class Posorneg {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n>0){
+			System.out.println("positive");
+		}
+		else if(n<0)
+		{
+			System.out.println("negative");
+		}
+		else
+		{
+			System.out.println("Invalid");
+		}
+
+	}
+
+}

80d9d4455704ecd869d9734bb330742f2be34fb2
ramprasanth11 <ram.prasanthcse@ksrce.ac.in>
2017-07-04 11:44:46 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class Leap {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int year=s.nextInt();
+		if(year%4==0){
+			System.out.println("it is leap year");
+		}
+		else
+		{
+			System.out.println("it is not a leapyear");
+		}
+
+	}
+
+}
